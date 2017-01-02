efba4fdf1e6f9a0d91834a31176a4c1d040c4e3c
Krister <krister.torsvik@gmail.com>
2017-01-02 18:54:06 +0100

    sending object

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function(req, res) {
-  res.send('hello world');
+  res.send({hello:'world'});
 });
 
 app.listen(3000, function() {
