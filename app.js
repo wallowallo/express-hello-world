15d8e930e9d46e519dff878b45b57e6663b5de32
Krister <krister.torsvik@gmail.com>
2017-01-12 15:17:17 +0100

    receiving from mongo db with mongoose

--- app.js
+++ app.js
@@ -5,36 +5,18 @@ var fs = require('fs');
 var path = require('path');
 var bodyParser = require('body-parser');
 var JSONStream = require('JSONStream');
-
-var todos = require('./todo.json')
-
-function getTodoFilePath () {
-  return path.join('./todo.json')
-}
-
-function saveTodo (data) {
-  var fp = getTodoFilePath()
-  fs.writeFileSync(fp, JSON.stringify(data, null, 2), {encoding: 'utf8'})
-}
+var Todo = require('./db').Todo
 
 app.use(bodyParser.json());
 
 app.get('/', function (req, res) {
-  var title = req.params.title
-  var description = req.params.description
-  var readable = fs.createReadStream('./todo.json')
-  readable.pipe(res)
+  Todo.find({}, function(err, todos) {
+    res.send({data:todos})
+  })
 });
 
 app.post('/', function(req, res) {
 });
-
-// app.put('/', function(req, res) {
-//   var title = req.params.title
-//   var description = req.params.description
-//   helpers.saveTodo(title, description) 
-//   res.end()
-// });
 
 app.listen(3000, function() {
   console.log('app listening on port 3000');
---
+++ db.js
@@ -0,0 +1,18 @@
+var uri = 'mongodb://localhost:27017/test'
+
+var mongoose = require('mongoose')
+mongoose.connect(uri)
+
+var db = mongoose.connection
+db.on('error', console.error.bind(console, 'connection error:'))
+db.once('open', function(callback) {
+  console.log('woop woop')
+})
+
+var todoSchema = mongoose.Schema({
+  title: String,
+  description: String
+})
+
+exports.Todo = mongoose.model('Todo', todoSchema)
+
